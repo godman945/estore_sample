162d717068640f3f0f2cbfb9c37655de16e1558a
alexchen3 <alexchen3@RNB20030254.fareastone.com.tw>
2021-02-02 18:30:57 +0800

    TEST

---
+++ src/main/java/estore_sample/estore/A.java
@@ -0,0 +1,5 @@
+package estore_sample.estore;
+
+public class A {
+
+}
--- src/main/java/estore_sample/estore/TestRun.java
+++ src/main/java/estore_sample/estore/TestRun.java
@@ -1,9 +1,27 @@
 package estore_sample.estore;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
 public class TestRun {
 
 	public static void main(String[] args) {
-		System.out.println("AAAAAA");
+		try{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String url = "jdbc:oracle:thin:@10.64.70.99:1530:estoreT3";    
+			String user = "es_dev";
+			String password = "es_dev";
+			Connection conn = DriverManager.getConnection(url, user, password);
+			
+			ResultSet resultSet = conn.createStatement().executeQuery("select * from co_master where 1=1 and rownum <3");
+			while (resultSet.next()) {
+			    String sid = resultSet.getString(2);
+			    System.out.println(sid);
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 
 }
