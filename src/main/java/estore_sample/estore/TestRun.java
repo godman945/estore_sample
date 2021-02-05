5be705655ee398787c57206a8d303d491d515fce
alexchen3 <alexchen3@RNB20030254.fareastone.com.tw>
2021-02-05 18:13:50 +0800

    更新

--- src/main/java/estore_sample/estore/TestRun.java
+++ src/main/java/estore_sample/estore/TestRun.java
@@ -1,6 +1,5 @@
 package estore_sample.estore;
 
-import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -9,16 +8,13 @@ import java.util.List;
 import java.util.Properties;
 import java.util.UUID;
 
-import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
-import com.fet.db.oracle.pojo.AalexMaster;
-import com.fet.db.oracle.pojo.EstoreTutorialAlex;
+import com.fet.db.oracle.pojo.EstoreTutorialAiko;
 
 public class TestRun {
 
@@ -33,7 +29,7 @@ public class TestRun {
 			String user = "es_dev";
 			String password = "es_dev";
 			Connection conn = DriverManager.getConnection(url, user, password);
-			ResultSet resultSet = conn.createStatement().executeQuery("select * from ESTORE_TUTORIAL_ALEX where 1=1 and rownum <3");
+			ResultSet resultSet = conn.createStatement().executeQuery("select * from ESTORE_TUTORIAL_AIKO where 1=1 and rownum <3");
 			while (resultSet.next()) {
 			    String sid = resultSet.getString(2);
 			    System.out.println(sid);
@@ -61,47 +57,45 @@ public class TestRun {
 			
 			 Configuration config = new Configuration();
 			 config.setProperties(settings);
-			 config.addAnnotatedClass(EstoreTutorialAlex.class);
+			 config.addAnnotatedClass(EstoreTutorialAiko.class);
 			 
 			 
 			 sessionFactory = config.buildSessionFactory();  
 			 session = sessionFactory.openSession();
 			 
 			 //原生sql
-			 Query query = session.createNativeQuery("select * from ESTORE_TUTORIAL_ALEX");
+			 Query query = session.createNativeQuery("select * from ESTORE_TUTORIAL_AIKO");
 			 System.out.println(query.list().size());
 
 			 //HQL
-			 query = session.createQuery(" from EstoreTutorialAlex");
-			 List<EstoreTutorialAlex> estoreTutorialAlexList = query.list();
-			 for (EstoreTutorialAlex estoreTutorialAlex : estoreTutorialAlexList) {
-				 System.out.println("ID:"+estoreTutorialAlex.getId());
-				 System.out.println("NAME:"+estoreTutorialAlex.getUserName());
+			 query = session.createQuery(" from EstoreTutorialAiko");
+			 List<EstoreTutorialAiko> estoreTutorialAikoList = query.list();
+			 for (EstoreTutorialAiko estoreTutorialAiko : estoreTutorialAikoList) {
+				 System.out.println("ID:"+estoreTutorialAiko.getId());
+				 System.out.println("NAME:"+estoreTutorialAiko.getUserName());
 			 }
 			 
 			 
 			 //新增
-//			 Transaction transaction  = session.beginTransaction();
-//			 EstoreTutorialAlex estoreTutorialAlex = new EstoreTutorialAlex();
-//			 estoreTutorialAlex.setId(UUID.randomUUID().toString());
-//			 estoreTutorialAlex.setUserName("TEST");
-//			 estoreTutorialAlex.setUserRemain(Long.valueOf("5"));
-//			 estoreTutorialAlex.setUserAddr("TTTT");
-//			 estoreTutorialAlex.setCreateTime(new Date());
-//			 estoreTutorialAlex.setUpdateTime(new Date());
-//			 session.save(estoreTutorialAlex);
-//			 session.flush();
-//			 transaction.commit();
-			 
-			 
-			 
 			 Transaction transaction  = session.beginTransaction();
-			 query = session.createQuery(" from EstoreTutorialAlex where userName =:findName ");    
-			 query.setParameter("findName", "TEST");
-			 List<EstoreTutorialAlex> findEstoreTutorialAlexList = query.list();
-			 if(findEstoreTutorialAlexList.size() == 1){
-				 EstoreTutorialAlex estoreTutorialAlex = findEstoreTutorialAlexList.get(0);
-				 System.out.println(estoreTutorialAlex.getUserAddr());
+			 EstoreTutorialAiko estoreTutorialAikoAdd = new EstoreTutorialAiko();
+			 estoreTutorialAikoAdd.setId(UUID.randomUUID().toString());
+			 estoreTutorialAikoAdd.setUserName("TEST");
+			 estoreTutorialAikoAdd.setUserRemain(Long.valueOf("5"));
+			 estoreTutorialAikoAdd.setUserAddr("TTTT");
+			 estoreTutorialAikoAdd.setCreateTime(new Date());
+			 estoreTutorialAikoAdd.setUpdateTime(new Date());
+			 session.save(estoreTutorialAikoAdd);
+			 session.flush();
+			 transaction.commit();
+			 
+			 transaction  = session.beginTransaction();
+			 query = session.createQuery(" from EstoreTutorialAiko where userName =:findName ");    
+			 query.setParameter("findName", "Alice");
+			 List<EstoreTutorialAiko> findEstoreTutorialAikoList = query.list();
+			 if(findEstoreTutorialAikoList.size() == 1){
+				 EstoreTutorialAiko estoreTutorialAiko = findEstoreTutorialAikoList.get(0);
+				 System.out.println(estoreTutorialAiko.getUserAddr());
 			 }
 			 
 			 
@@ -118,7 +112,7 @@ public class TestRun {
 	
 	public static void main(String[] args) {
 		TestRun testRun = new TestRun();
-//		testRun.testOracleConnection();
+		testRun.testOracleConnection();
 		testRun.testHibernate();
 	}
 
